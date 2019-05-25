938f1e1ff0d073aa30139dbd018208c3caadbdd6
jkanchelov <jkanchelov@gmail.com>
2019-05-25 22:04:53 +0300

    Update local get-questions to not give correct answers

--- src/app/models/services/questions/local/get-questions.js
+++ src/app/models/services/questions/local/get-questions.js
@@ -1,7 +1,12 @@
-import questions from "./questions"; 
+import questionsData from "./questionsData";
 
 export default {
     getQuestions: () => {
-        return questions;
+        return questionsData.map(question => {
+            return {
+                question: question.question,
+                answer: question.answer
+            };
+        });
     }
 };
